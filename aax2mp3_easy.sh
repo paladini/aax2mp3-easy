c6727845cf4f6474c6da15e7d2ca881b4144b995
Fernando Paladini <fnpaladini@gmail.com>
2016-10-13 23:01:16 -0300

    Big changes in project architecture.

--- aax2mp3_easy.sh
+++ aax2mp3_easy.sh
@@ -1,34 +1,46 @@
 #!/bin/sh
 
+LOGIN=$1
+PASSWORD=$2
+FILES=$3
+
 #------------------
 # Set-up AAXtoMP3
 #------------------
-# Download AAX to MP3
-wget https://github.com/KrumpetPirate/AAXtoMP3/archive/master.zip -O downloads/aaxtomp3.zip
-
-# Extract to folder using 'unzip'
-unzip downloads/aaxtomp3.zip -d aaxtomp3/
-
+if [ ! -d "AAXtoMP3-master" ]; then
+	echo "Downloading AAXtoMP3..."
+	wget https://github.com/KrumpetPirate/AAXtoMP3/archive/master.zip -O aaxtomp3.zip
+	unzip aaxtomp3.zip
+	rm aaxtomp3.zip
+fi
 
 #--------------------------
 # Set-up Audible-Activator
 #--------------------------
-# Download
-wget https://github.com/inAudible-NG/audible-activator/archive/master.zip -O downloads/audible-activator.zip
+if [ ! -d "audible-activator-master" ]; then
+	echo "Downloading audible-activator..."
+	wget https://github.com/inAudible-NG/audible-activator/archive/master.zip -O audible-activator.zip
+	unzip audible-activator.zip
+	rm audible-activator.zip
 
-# Extract to folder using 'unzip'
-unzip downloads/audible-activator.zip -d activator/
+	echo "Downloading Chrome-Driver-Latest..."
+	wget https://chromedriver.storage.googleapis.com/LATEST_RELEASE -O chromedriver-latest-release.txt
+	LATEST_CHROMEDRIVER_VERSION=$(cat chromedriver-latest-release.txt)
+	wget https://chromedriver.storage.googleapis.com/$LATEST_CHROMEDRIVER_VERSION/chromedriver_linux64.zip -O chrome-driver-latest.zip
+	unzip chrome-driver-latest.zip
+	rm chrome-driver-latest.zip
+fi
 
-# Downloading prerequisites (Requests, Selenium, ChromeDriver)
+# Downloading prerequisites (Requests, Selenium)
 sudo pip install requests
 sudo pip install selenium
-wget https://chromedriver.storage.googleapis.com/LATEST_RELEASE -O downloads/chromedriver-latest-release.txt
-LATEST_CHROMEDRIVER_VERSION=$(cat downloads/chromedriver-latest-release.txt)
-wget https://chromedriver.storage.googleapis.com/index.html?path=$LATEST_CHROMEDRIVER_VERSION/chromedriver_linux64.zip -O downloads/chrome-driver-latest.zip
-unzip downloads/chrome-driver-latest.zip -d activator/
 
-# Activation code
-OUTPUT=$(exif python activator/audible-activator.py | tail -1)
+#--------------------------
+# Getting activation code
+#--------------------------
+OUTPUT=$(python audible-activator-master/audible-activator.py | tail -1)
 
-# Converting AAX to MP3
-bash aaxtomp3/AAXtoMP3.sh $OUTPUT 
+#--------------------------
+# Converting files
+#--------------------------
+bash AAXtoMP3-master/AAXtoMP3.sh $OUTPUT $1
